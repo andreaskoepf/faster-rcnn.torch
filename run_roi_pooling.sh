cd6c00cb7e7f7c52a664680f59df05475ca42b87
ialtrogge <inga.altrogge@xamla.com>
2016-07-14 15:12:20 +0200

    Update run_roi_pooling.sh

--- run_roi_pooling.sh
+++ run_roi_pooling.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Note: "trained_on_imagenet_004000.t7" can be found on "firefly" at "/home/altrogge/code/27_06_2016/faster-rcnn.torch/" !!!
+
 #th perform_ROIPoolingMassa_singleImg.lua -cfg config/imagenet.lua -name imagenet1 -train ILSVRC2015_DET.t7 -resultDir logs6
 
 #th perform_ROIPoolingMassa_singleImg.lua -cfg config/imagenet.lua -name imagenet1 -train ILSVRC2015_DET.t7 -resultDir logs6 -restore trained_on_imagenet_004000.t7
