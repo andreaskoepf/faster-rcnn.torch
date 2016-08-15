2cde5d14aa249842fe7a7e37f20b5a77e34111f1
ialtrogge <inga.altrogge@xamla.com>
2016-08-15 13:32:59 +0200

    add training mode flag (onlyPnet, onlyCnet, both)

--- Detector.lua
+++ Detector.lua
@@ -69,117 +69,80 @@ function Detector:detect(input)
 
   end
 
-  return matches
+  if mode == 'onlyPnet' then
+    return matches
+  else
+    local winners = {}
+    if #matches > 0 then
 
-  -- OLD IMPL ...
+      -- NON-MAXIMUM SUPPRESSION
+      local bb = torch.Tensor(#matches, 4)
+      local score = torch.Tensor(#matches, 1)
+      for i=1,#matches do
+        bb[i] = matches[i].r:totensor()
+        score[i] = matches[i].p
+      end
 
-  --[[local aspect_ratios = 3
-  for i=1,#outputs do
-    local layer = outputs[i]
-    local layer_size = layer:size()
-    for y=1,layer_size[2] do
-      for x=1,layer_size[3] do
-        local c = layer[{{}, y, x}]
-        for a=1,aspect_ratios do
+      local iou_threshold = 0.25 --FIXME =0.25
+      local pick = nms(bb, iou_threshold, score)
+      --local pick = nms(bb, iou_threshold, 'area')
+      local candidates = {}
+      pick:apply(function (x) table.insert(candidates, matches[x]) end )
 
-          local ofs = (a-1) * 6
-          local cls_out = c[{{ofs + 1, ofs + 2}}]
-          local reg_out = c[{{ofs + 3, ofs + 6}}]
+      --print(string.format('[Detector:detect] candidates: %d', #candidates))
 
-          -- classification
-          local c = lsm:forward(cls_out)
-          --if c[1] > c[2] then
-          local c_norm = m:forward(cls_out)
-          --if math.exp(c[1]) > 0.9 then  -- only two classes (foreground and background)
+      -- REGION CLASSIFICATION
+      cnet:evaluate()
 
-          if c_norm[1] > 0.7 then  -- only two classes (foreground and background)
+      -- create cnet input batch
+      local cinput = torch.CudaTensor(#candidates, cfg.roi_pooling.kw * cfg.roi_pooling.kh * cnet_input_planes)
+      for i,v in ipairs(candidates) do
+        -- pass through adaptive max pooling operation
+        local pi, idx = extract_roi_pooling_input(v.r, self.localizer, outputs[#outputs])
+        cinput[i] = amp:forward(pi):view(cfg.roi_pooling.kw * cfg.roi_pooling.kh * cnet_input_planes)
+      end
 
-            -- regression
-            local a_ = self.anchors:get(i,a,y,x)
-            local r = Anchors.anchorToInput(a_, reg_out)
-            if r:overlaps(input_rect) then
-              table.insert(matches, { p=c_norm[1], a=a_, r=r, l=i })
-            end
+      -- send extracted roi-data through classification network
+      local coutputs = cnet:forward(cinput)
+      local bbox_out = coutputs[1]
+      local cls_out = coutputs[2]
+      local c_norm= m:forward(cls_out)
+      local yclass = {}
+      for i,x in ipairs(candidates) do
+        x.r2 = Anchors.anchorToInput(x.r, bbox_out[i])
+
+        local cprob = c_norm[i]
+        local p,c = torch.sort(cprob, 1, true) -- get probabilities and class indicies
+
+        x.class = c[1]
+        x.confidence = p[1]
+        -- print(x.class)
+        if x.class ~= bgclass and math.exp(x.confidence) > 0.2 then
+          --if x.class ~= bgclass and x.confidence > 0.2 then
+          if not yclass[x.class] then
+            yclass[x.class] = {}
           end
 
+          table.insert(yclass[x.class], x)
         end
       end
-    end
-  end
-]]
 
---[[  local winners = {}
-
-  if #matches > 0 then
-
-    -- NON-MAXIMUM SUPPRESSION
-    local bb = torch.Tensor(#matches, 4)
-    local score = torch.Tensor(#matches, 1)
-    for i=1,#matches do
-      bb[i] = matches[i].r:totensor()
-      score[i] = matches[i].p
-    end
-
-    local iou_threshold = 0.25 --FIXME =0.25
-    local pick = nms(bb, iou_threshold, score)
-    --local pick = nms(bb, iou_threshold, 'area')
-    local candidates = {}
-    pick:apply(function (x) table.insert(candidates, matches[x]) end )
-
-    --print(string.format('[Detector:detect] candidates: %d', #candidates))
-
-    -- REGION CLASSIFICATION
-    cnet:evaluate()
-
-    -- create cnet input batch
-    local cinput = torch.CudaTensor(#candidates, cfg.roi_pooling.kw * cfg.roi_pooling.kh * cnet_input_planes)
-    for i,v in ipairs(candidates) do
-      -- pass through adaptive max pooling operation
-      local pi, idx = extract_roi_pooling_input(v.r, self.localizer, outputs[#outputs])
-      cinput[i] = amp:forward(pi):view(cfg.roi_pooling.kw * cfg.roi_pooling.kh * cnet_input_planes)
-    end
-
-    -- send extracted roi-data through classification network
-    local coutputs = cnet:forward(cinput)
-    local bbox_out = coutputs[1]
-    local cls_out = coutputs[2]
-    local c_norm= m:forward(cls_out)
-    local yclass = {}
-    for i,x in ipairs(candidates) do
-      x.r2 = Anchors.anchorToInput(x.r, bbox_out[i])
-
-      local cprob = c_norm[i]
-      local p,c = torch.sort(cprob, 1, true) -- get probabilities and class indicies
-
-      x.class = c[1]
-      x.confidence = p[1]
-      -- print(x.class)
-      if x.class ~= bgclass and math.exp(x.confidence) > 0.2 then
-        --if x.class ~= bgclass and x.confidence > 0.2 then
-        if not yclass[x.class] then
-          yclass[x.class] = {}
+      local overlab = 0.2
+      -- run per class NMS
+      for i,c in pairs(yclass) do
+        -- fill rect tensor
+        bb = torch.Tensor(#c, 5)
+        for j,r in ipairs(c) do
+          bb[{j, {1,4}}] = r.r2:totensor()
+          bb[{j, 5}] = r.confidence
         end
 
-        table.insert(yclass[x.class], x)
+        pick = nms(bb, overlab, bb[{{}, 5}])
+        pick:apply(function (x) table.insert(winners, c[x]) end )
+
       end
     end
 
-    local overlab = 0.2
-    -- run per class NMS
-    for i,c in pairs(yclass) do
-      -- fill rect tensor
-      bb = torch.Tensor(#c, 5)
-      for j,r in ipairs(c) do
-        bb[{j, {1,4}}] = r.r2:totensor()
-        bb[{j, 5}] = r.confidence
-      end
-
-      pick = nms(bb, overlab, bb[{{}, 5}])
-      pick:apply(function (x) table.insert(winners, c[x]) end )
-
-    end
-
-  end
-
-  return winners]]
+    return winners
+  end -- if mode ~= 'onlyPnet' then
 end
