635be4de3d03233ddc5ff2aa721dc15f40edd583
A. Lemme <andre.lemme@xamla.com>
2016-09-06 12:10:36 +0200

    added uniform_scaling

--- config/pascal_voc.lua
+++ config/pascal_voc.lua
@@ -3,17 +3,18 @@
   target_smaller_side = 224,
   scales = { 48, 96, 172 },
   max_pixel_size = 1000,
-  normalization = { centering = false, scaling = false },
+  normalization = {},--{ centering = false, scaling = false },
   augmentation = { vflip = 0.0, hflip = 0.0, random_scaling = 0.0, aspect_jitter = 0.0 },
   color_space = 'rgb',
   roi_pooling = { kw = 3, kh = 3 },
   examples_base_path = '/data/VOC2007/',
   background_base_path = nil,
-  batch_size = 256,
+  batch_size = 128,
   positive_threshold = 0.7, 
   negative_threshold = 0.3,
   best_match = false,
-  nearby_aversion = true,
+  nearby_aversion = false,
+  uniform_image_scaling = true, 
   backgroundClass = 1
 }
 
